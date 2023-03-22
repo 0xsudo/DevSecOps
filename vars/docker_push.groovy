194f75d66009c4c81e1f3c787410aae9d728a22e
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-22 15:42:41 +0300

    parsed push to ecr vars as parameters

--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -1,7 +1,7 @@
 def call(Map param = [:]) {
     retry(count: 3) {
 		if (params.ecr_action == 'create') {
-			docker.withRegistry('https://636181284446.dkr.ecr.us-east-1.amazonaws.com', 'ecr:${param.region}:${param.IAMrole}') {
+			docker.withRegistry('https://636181284446.dkr.ecr.us-east-1.amazonaws.com', 'ecr:${param.region}:${param.iamrole}') {
 				docker_image.push('${param.tag}')
 			}
 		}
