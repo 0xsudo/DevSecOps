b4a7536c80c954bcab2f5700f8e0875eda1fff89
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-22 19:21:10 +0300

    updated map place holder

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,9 +1,9 @@
-def call(Map param = [:]) {
+def call(Map config = [:]) {
     retry(count: 3) {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
-					docker_image=docker.build('${param.name}')
+					docker_image=docker.build('${config.name}')
 				}
 			}
 		}
