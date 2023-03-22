bc104cabc8f109596b76e86095b3b57335f7b85c
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-22 15:00:59 +0300

    parsed image name as parameter

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -1,9 +1,9 @@
-def call() {
+def call(Map params = [:]) {
     retry(count: 3) {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
-					docker_image=docker.build('buggy-app')
+					docker_image=docker.build('${params.name}')
 				}
 			}
 		}
