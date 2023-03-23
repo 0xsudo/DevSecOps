c8233f1b7aa57190c0bbf7296fef5f381275c64c
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-23 15:46:35 +0300

    parsed updated  image name as var

--- vars/docker_build.groovy
+++ vars/docker_build.groovy
@@ -3,7 +3,7 @@ def call() {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
-					docker.build('buggyapp')
+					img = docker.build('buggyapp')
 				}
 			}
 		}
--- vars/docker_push.groovy
+++ vars/docker_push.groovy
@@ -2,7 +2,7 @@ def call(Map config = [:]) {
     retry(count: 3) {
 		if (params.ecr_action == 'create') {
 			docker.withRegistry('https://636181284446.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:devopsrole') {
-				buggyapp.push('${config.tag}')
+				img.push('${config.tag}')
 			}
 		}
 	}
