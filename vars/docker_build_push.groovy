fa218ec53198cd2ca1a0026f789c81e5abe3b08b
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-24 23:15:57 +0300

    using docker shell commands

--- vars/docker_build_push.groovy
+++ vars/docker_build_push.groovy
@@ -3,7 +3,7 @@ def call() {
 		withDockerRegistry([credentialsId: 'docker-login', url: '']) {
 			script {
 				if (params.ecr_action == 'create') {
-					sh 'docker build buggyapp .'
+					sh 'docker build -t buggyapp .'
 					sh 'docker tag buggyapp https://636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
 					sh 'docker push https://636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
 				}
