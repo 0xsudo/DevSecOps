9aa64e7365ad47b9a6730c167c23a5041adf1240
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-25 09:57:04 +0300

    fixed docker tag

--- vars/docker_build_push.groovy
+++ vars/docker_build_push.groovy
@@ -4,8 +4,8 @@ def call() {
 			script {
 				if (params.ecr_action == 'create') {
 					sh 'docker build -t buggyapp .'
-					sh 'docker tag buggyapp https://636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
-					sh 'docker push https://636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
+					sh 'docker tag buggyapp 636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
+					sh 'docker push 636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
 				}
 			}
 		}
