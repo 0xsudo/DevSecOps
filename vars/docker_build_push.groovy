e54fc1b735393ddd21637cf379c6e4d27c9fae25
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-25 10:24:34 +0300

    parsed ecr creds with correct syntax

--- vars/docker_build_push.groovy
+++ vars/docker_build_push.groovy
@@ -5,7 +5,7 @@ def call() {
 				if (params.ecr_action == 'create') {
 					sh 'docker build -t buggyapp .'
 					sh 'docker tag buggyapp 636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
-					docker.withRegistry('https://636181284446.dkr.ecr.us-east-1.amazonaws.com', 'devopsrole') {
+					docker.withRegistry('https://636181284446.dkr.ecr.us-east-1.amazonaws.com', 'ecr:us-east-1:devopsrole') {
 						sh 'docker push 636181284446.dkr.ecr.us-east-1.amazonaws.com/buggyapp:latest'
 					}
 				}
