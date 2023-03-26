6e57fb4687073ac738428a28a9d194a5cd24461e
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-26 15:57:52 +0300

    fixed data type

--- vars/static_sleep.groovy
+++ vars/static_sleep.groovy
@@ -1,4 +1,4 @@
-def call(Int period) {
+def call(int period) {
     if (params.eksctl_action == 'create' && params.ecr_action == 'create') {
 		sh 'sleep $period; echo "Deployment ready for DAST analysis on EKS"'
 	}
