99cfd058ffa9c1d9af9f9ced92d234ad044623d1
0xsudo <kaokaonchangwakelvin@gmail.com>
2023-03-27 17:09:12 +0300

    removed shared libs

--- vars/eks_connect.groovy
+++
@@ -1,6 +0,0 @@
-def call(Map config = [:]) {
-    if (params.eksctl_action == 'create' && params.ecr_action == 'create') {
-		sh 'aws eks update-kubeconfig --region ${config.region} --name ${config.clustername}'
-		sh 'sleep 120'
-	}
-}
